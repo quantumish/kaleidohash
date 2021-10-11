0eda6d2d9572ec9af7035113a6c0ec475990c865
David Freifeld <freifeld.david@gmail.com>
2021-10-10 19:41:24 -0700

    Reduce functions remain broken

--- src/kaleidohash.rs
+++ src/kaleidohash.rs
@@ -1,6 +1,4 @@
-
-
-use rand::{thread_rng, Rng};
+use rand::{thread_rng, Rng, seq::SliceRandom};
 use rand::distributions::Alphanumeric;
 use std::time::Instant;
 use std::fmt;
@@ -14,38 +12,33 @@ use indicatif::ProgressBar;
 
 const HASH_SIZE: usize = 20;
 type Hash = [u8; HASH_SIZE];
-const PASS_SIZE: usize = 6; // TODO REMOVE
+const PASS_SIZE: usize = 3; // TODO REMOVE
 
-// fn alpha_to_ascii(s: Hash) -> Vec<u8> {
-//     let mut out: Vec<u8> = Vec::with_capacity(PASS_SIZE);
+struct Charset;
+
+impl rand::distributions::Distribution<u8> for Charset {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> u8 {
+	*"0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz".to_string().into_bytes()
+	    .choose(rng).unwrap()
+    }
+}
+
+// fn alpha_to_u128(s: Vec<u8>) -> u128 {
+//     let mut out: u128 = 0;
 //     for i in 0..s.len() {
-// 	if s[i] <= 9 {
-// 	    out.push(s[i]+48)
-// 	} else if s[i] <= 35 {
-// 	    out.push(s[i]-10+65)
-// 	} else {
-// 	    out.push(s[i]-36+97)
+// 	if s[i] <= 48+9 && s[i] >= 48 {
+// 	    println!("{} {}", s[i], s[i]-48);
+// 	    out += (s[i]-48) as u128 * 62u128.pow(i as u32);
+// 	} else if s[i] <= 65+26 && s[i] >= 65 {
+// 	    println!("{} {}", s[i], s[i]-65+10);
+// 	    out += (s[i]-65+10) as u128 * 62u128.pow(i as u32);
+// 	} else if s[i] <= 97+26 && s[i] >= 97 {
+// 	    println!("{} {}", s[i], s[i]-97+36);
+// 	    out += (s[i]-97+36) as u128 * 62u128.pow(i as u32);
 // 	}
 //     }
 //     out
 // }
-
-fn alpha_to_u128(s: Vec<u8>) -> u128 {
-    let mut out: u128 = 0;
-    for i in 0..s.len() {
-	if s[i] <= 48+9 && s[i] >= 48 {
-	    println!("{} {}", s[i], s[i]-48);
-	    out += (s[i]-48) as u128 * 62u128.pow(i as u32);
-	} else if s[i] <= 65+26 && s[i] >= 65 {
-	    println!("{} {}", s[i], s[i]-65+10);
-	    out += (s[i]-65+10) as u128 * 62u128.pow(i as u32);
-	} else if s[i] <= 97+26 && s[i] >= 97 {
-	    println!("{} {}", s[i], s[i]-97+36);
-	    out += (s[i]-97+36) as u128 * 62u128.pow(i as u32);
-	}
-    }
-    out
-}
 
 #[derive(Serialize, Deserialize)]
 struct RainbowChain {
@@ -73,7 +66,15 @@ impl RainbowChain {
 
 // TODO Sketchy implementation
 fn reduce(hash: &Hash, deriv: usize) -> Vec<u8> {
-    hash.map(|i| (((i as u32 + deriv as u32) % (75)) + 48) as u8)[..PASS_SIZE].to_vec()
+    let mut rng: rand_pcg::Pcg64 = rand_seeder::Seeder::from(hash).make_rng();
+    rng.sample_iter(&Charset)
+	.take(PASS_SIZE)
+	.collect()
+    // for i in 0..PASS_SIZE {
+	
+    // }
+    // hash.map(|i| (((i as u32 + deriv as u32 + rng.gen::<u32>()
+    // ) % (75)) + 48) as u8)[..PASS_SIZE].to_vec()
     /*
     let mut out: Vec<u8> = Vec::new();
     for j in 0..pass_size {
@@ -111,7 +112,7 @@ impl RainbowTable {
 	for _i in 0..r.info.num_chains {
 	    loop {
 		let plaintext: Vec<u8> = thread_rng()
-		    .sample_iter(&Alphanumeric)
+		    .sample_iter(&Charset)
 		    .take(r.info.pass_size)
 		    .collect();
 		if initials.insert(plaintext.clone()) {		    
@@ -125,7 +126,7 @@ impl RainbowTable {
         progress.enable_steady_tick(250);	
         let n = AtomicU64::new(0);
 	
-	r.chains = r.chains.iter()
+	r.chains = r.chains.par_iter()
 	    .map(|i| RainbowChain::forward(i.initial.clone(),
 					   r.info.chain_len,
 					   &progress,
@@ -206,21 +207,22 @@ impl fmt::Display for RainbowTable {
             .with_units("B")
             .format(bytes as f64);
         write!(f, "{} {}x{} rainbow table with {} hashes.", size,
-	       self.info.num_chains, self.info.chain_len, (self.info.chain_len/2)*self.info.num_chains)
+	       self.info.num_chains, self.info.chain_len, Formatter::new().format(((self.info.chain_len/2)*self.info.num_chains) as f64))
     }
 }
 
 fn main() {
+    println!("Generating rainbow table...");
     let s = Instant::now();
-    let r: RainbowTable = RainbowTable::new(2000, 40000, 6);
+    let r: RainbowTable = RainbowTable::new(2000, 40000, PASS_SIZE);
     println!("{} in {:?}", r, s.elapsed());
     println!("{} duplicates out of {} rows.", r.duplicates(), r.info.num_chains);
-    let targets: Vec<Hash> = vec![sha1(b"passwd"),
-				  sha1(b"t3st/n"),
-				  sha1(b"p4sswd"),
-				  sha1(b"ABdw32"),
-				  sha1(b"ABc123"),
-				  sha1(b"fuTur3")];
+    let targets: Vec<Hash> = vec![sha1(b"ps\\"),
+				  sha1(b"ts3"),
+				  sha1(b"ps4"),
+				  sha1(b"ABs"),
+				  sha1(b"ABc"),
+				  sha1(b"fu4")];
     for target in targets.iter() {
 	let start = Instant::now();	
 	match r.lookup(target.clone()) {
